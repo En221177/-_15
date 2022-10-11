533fd2fae7eae191687b1a50cc4edf97e5df0145
En Yang <y.joseph0217@gmail.com>
2022-10-11 19:53:16 +0800

    done

--- 楊承恩_15/HOMEWORK.Designer.cs
+++ 楊承恩_15/HOMEWORK.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HOMEWORK));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -40,89 +41,100 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button1.Location = new System.Drawing.Point(34, 71);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(241, 59);
             this.button1.TabIndex = 0;
             this.button1.Text = "FirstForm";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button2.Location = new System.Drawing.Point(34, 153);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(241, 59);
             this.button2.TabIndex = 1;
             this.button2.Text = "利息";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button3.Location = new System.Drawing.Point(34, 235);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(241, 59);
             this.button3.TabIndex = 2;
             this.button3.Text = "calculator";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button4.Location = new System.Drawing.Point(34, 324);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(241, 59);
             this.button4.TabIndex = 3;
             this.button4.Text = "guess";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
+            this.button5.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button5.Location = new System.Drawing.Point(34, 419);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(241, 59);
             this.button5.TabIndex = 4;
             this.button5.Text = "OOXX";
-            this.button5.UseVisualStyleBackColor = true;
+            this.button5.UseVisualStyleBackColor = false;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
+            this.button6.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button6.Location = new System.Drawing.Point(34, 520);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(241, 59);
             this.button6.TabIndex = 5;
             this.button6.Text = "Pos";
-            this.button6.UseVisualStyleBackColor = true;
+            this.button6.UseVisualStyleBackColor = false;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
+            this.button7.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button7.Location = new System.Drawing.Point(34, 617);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(241, 59);
             this.button7.TabIndex = 6;
             this.button7.Text = "StudendStruct";
-            this.button7.UseVisualStyleBackColor = true;
+            this.button7.UseVisualStyleBackColor = false;
             this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
+            this.button8.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button8.Location = new System.Drawing.Point(34, 710);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(241, 59);
             this.button8.TabIndex = 7;
             this.button8.Text = "studentGrade";
-            this.button8.UseVisualStyleBackColor = true;
+            this.button8.UseVisualStyleBackColor = false;
             this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // HOMEWORK
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1909, 1018);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1285, 964);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
@@ -133,6 +145,7 @@
             this.Controls.Add(this.button1);
             this.Name = "HOMEWORK";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
 
         }
